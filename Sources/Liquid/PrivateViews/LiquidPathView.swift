a34fb117cad6dbc9489f8d20e33f95195e5537ea
borsosbe <borsosbe@gmail.com>
2024-08-18 08:50:10 +0200

    Update LiquidPathView.swift
    remove combine trigger from LiquidPathView

--- Sources/Liquid/PrivateViews/LiquidPathView.swift
+++ Sources/Liquid/PrivateViews/LiquidPathView.swift
@@ -16,34 +16,35 @@ struct LiquidPathView: View {
     @State var y: AnimatableArray = .zero
     @State var samples: Int
     let period: TimeInterval
-    let trigger: Timer.TimerPublisher
-
-    var cancellable: Cancellable?
 
     init(path: CGPath, interpolate: Int, samples: Int, period: TimeInterval) {
         self._samples = .init(initialValue: samples)
         self.period = period
-        self.trigger = Timer.TimerPublisher(interval: period, runLoop: .main, mode: .common)
-        self.cancellable = self.trigger.connect()
         self.pointCloud = path.getPoints().interpolate(interpolate)
     }
 
     func generate() {
-        withAnimation(.linear(duration: period)) {
-            let points = Array(0..<pointCloud.x.count).randomElements(samples)
-            self.x = AnimatableArray(points.map { self.pointCloud.x[$0] })
-            self.y = AnimatableArray(points.map { self.pointCloud.y[$0] })
+        if #available(iOS 17.0, *) {
+            withAnimation(.linear(duration: period)) {
+                let points = Array(0..<pointCloud.x.count).randomElements(samples)
+                self.x = AnimatableArray(points.map { self.pointCloud.x[$0] })
+                self.y = AnimatableArray(points.map { self.pointCloud.y[$0] })
+            } completion: {
+                self.generate()
+            }
+        } else {
+            withAnimation(.linear(duration: period)) {
+                let points = Array(0..<pointCloud.x.count).randomElements(samples)
+                self.x = AnimatableArray(points.map { self.pointCloud.x[$0] })
+                self.y = AnimatableArray(points.map { self.pointCloud.y[$0] })
+            }
         }
     }
 
     var body: some View {
         LiquidPath(x: x, y: y)
-            .onReceive(trigger) { _ in
+            .onAppear {
                 self.generate()
-            }.onAppear {
-                self.generate()
-            }.onDisappear {
-                self.cancellable?.cancel()
             }
     }
 }
